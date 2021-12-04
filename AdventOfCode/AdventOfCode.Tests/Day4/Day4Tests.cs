6db9a53f383a1ac4bcb4028d6af1093ad3d2fbbf
Andre Maxime <maxime.andre@zenika.com>
2021-12-04 10:08:47 +0100

    init day4

---
+++ AdventOfCode/AdventOfCode.Tests/Day4/Day4Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day4Tests
+    {
+        private ITestOutputHelper _output;
+        public Day4Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/test.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(4512);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/input.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day4
+    {
+
+        public int Compute(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            return 0;
+        }
+    }
+}
