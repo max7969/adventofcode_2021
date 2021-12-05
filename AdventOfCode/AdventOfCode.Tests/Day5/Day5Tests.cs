b388608a63f284b1b247b210c97afbce863de1f2
maxime.andre <maxime.andr@gmail.com>
2021-12-05 10:01:20 +0100

    day5 part 1 & 2

---
+++ AdventOfCode/AdventOfCode.Tests/Day5/Day5Tests.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day5Tests
+    {
+        private ITestOutputHelper _output;
+        public Day5Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/test.txt";
+            Day5 day = new Day5();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(5);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/input.txt";
+            Day5 day = new Day5();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/test.txt";
+            Day5 day = new Day5();
+
+            // Act
+            int result = day.Compute(filePath, true);
+
+            // Assert
+            result.Should().Be(12);
+        }
+
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/input.txt";
+            Day5 day = new Day5();
+
+            // Act
+            int result = day.Compute(filePath, true);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Day5/Day5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day5
+    {
+        private class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        public int Compute(string filePath, bool includeDiagonals = false)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            var winds = input.Select(x =>
+                x.Split(" -> ").Select(y => new Point() {X = int.Parse(y.Split(",")[0]), Y = int.Parse(y.Split(",")[1]) }).ToArray());
+
+            var map = new Dictionary<string, int>();
+
+            foreach (var wind in winds)
+            {
+
+                if (wind[0].X == wind[1].X)
+                {
+                    for (int i = wind.Select(x => x.Y).Min(); i <= wind.Select(x => x.Y).Max(); i++)
+                    {
+                        AddPointToMap(map, $"{wind[0].X},{i}");
+                        
+                    }
+                } 
+                else if (wind[0].Y == wind[1].Y)
+                {
+                    for (int i = wind.Select(x => x.X).Min(); i <= wind.Select(x => x.X).Max(); i++)
+                    {
+                        AddPointToMap(map, $"{i},{wind[0].Y}");
+                    }
+                }
+                else if (includeDiagonals)
+                {
+                    var sortedWind = wind.OrderBy(x => x.X).ToArray();
+                    if (sortedWind[0].Y < sortedWind[1].Y)
+                    {
+                        do
+                        {
+                            AddPointToMap(map,$"{sortedWind[0].X++},{sortedWind[0].Y++}");
+                        } while (sortedWind[0].X != sortedWind[1].X + 1);
+                    }
+                    else
+                    {
+                        do
+                        {
+                            AddPointToMap(map, $"{sortedWind[0].X++},{sortedWind[0].Y--}");
+                        } while (sortedWind[0].X != sortedWind[1].X + 1);
+                    }
+                }
+            }
+
+            return map.Count(x => x.Value > 1);
+        }
+
+        private void AddPointToMap(Dictionary<string, int> map, string key)
+        {
+            if (map.ContainsKey(key))
+            {
+                map[key] += 1;
+            }
+            else
+            {
+                map.Add(key, 1);
+            }
+        }
+    }
+}
