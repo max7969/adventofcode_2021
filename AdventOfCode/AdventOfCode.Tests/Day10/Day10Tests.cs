767e1f84530cf3a3b99d9ccb754c1346766af64e
maxime.andre <maxime.andr@gmail.com>
2021-12-10 09:06:25 +0100

    day10 part 1 & 2

---
+++ AdventOfCode/AdventOfCode.Tests/Day10/Day10Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day10Tests
+    {
+        private ITestOutputHelper _output;
+        public Day10Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day10/Resources/test.txt";
+            Day10 day = new Day10();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(26397);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day10/Resources/input.txt";
+            Day10 day = new Day10();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day10/Resources/test.txt";
+            Day10 day = new Day10();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(288957);
+        }
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day10/Resources/input.txt";
+            Day10 day = new Day10();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Day10/Day10.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day10
+    {
+        public int Compute(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            var result = 0;
+            foreach(var line in input)
+            {
+                string copyLine = TreatLine(line);
+                if (IsCorruptedLine(copyLine))
+                {
+                    result += _points[copyLine.First(x => _points.Keys.Contains(x))];
+                }
+            }
+            return result;
+        }
+
+        public long Compute2(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            var scores = new List<long>();
+            foreach (var line in input)
+            {
+                string copyLine = TreatLine(line);
+                if (IsIncompleteLine(copyLine))
+                {
+                    long score = 0;
+                    foreach(char c in copyLine.Reverse())
+                    {
+                        score = score * 5 + _completePoints[c];
+                    }
+                    scores.Add(score);
+                }
+            }
+            return scores.OrderBy(x => x).ToList()[scores.Count / 2];
+        }
+
+        private string TreatLine(string line)
+        {
+            var previousLength = line.Length + 1;
+            while (line.Length > 0 && line.Length < previousLength)
+            {
+                previousLength = line.Length;
+                for (int i = 0; i < 4; i++)
+                {
+                    line = line.Replace($"{_completePoints.Keys.ToList()[i]}{_points.Keys.ToList()[i]}", "");
+                }
+            }
+            return line;
+        }
+
+        private bool IsCorruptedLine(string line)
+        {
+            return line.Length > 0 && line.ToCharArray().Any(x => _points.Keys.Contains(x));
+        }
+
+        private bool IsIncompleteLine(string line)
+        {
+            return line.Length > 0 && !line.ToCharArray().Any(x => _points.Keys.Contains(x));
+        }
+
+        Dictionary<char, int> _points = new Dictionary<char, int>
+        {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 }
+        };
+
+        Dictionary<char, int> _completePoints = new Dictionary<char, int>
+        {
+            { '(', 1 },
+            { '[', 2 },
+            { '{', 3 },
+            { '<', 4 }
+        };
+    }
+}
