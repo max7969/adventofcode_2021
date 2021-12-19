ba2ef48f0fe726d6be5240ad525505912e209974
maxime.andre <maxime.andr@gmail.com>
2021-12-19 10:00:11 +0100

    day18 part 1 & 2

--- AdventOfCode/AdventOfCode.Tests/Day18/Day18Tests.cs
+++ AdventOfCode/AdventOfCode.Tests/Day18/Day18Tests.cs
@@ -15,8 +15,25 @@ namespace AdventOfCode.Tests
             _output = output;
         }
 
+
+
+        [Theory]
+        [InlineData("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+        [InlineData("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+        [InlineData("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+        [InlineData("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+        [InlineData("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+        public void Test1Part1(string initialChain, string resultChain)
+        {
+            // Act
+            string result = Day18.CycleReduce(initialChain);
+
+            // Assert
+            result.Should().Be(resultChain);
+        }
+
         [Fact]
-        public void Test1Part1()
+        public void Test2Part1()
         {
             // Arrange 
             string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day18/Resources/test.txt";
@@ -26,7 +43,21 @@ namespace AdventOfCode.Tests
             long result = day.Compute(filePath);
 
             // Assert
-            result.Should().Be(45);
+            result.Should().Be(3488);
+        }
+
+        [Fact]
+        public void Test3Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day18/Resources/test2.txt";
+            Day18 day = new Day18();
+
+            // Act
+            long result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(4140);
         }
 
         [Fact]
@@ -38,6 +69,34 @@ namespace AdventOfCode.Tests
 
             // Act
             long result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day18/Resources/test2.txt";
+            Day18 day = new Day18();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(3993);
+        }
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day18/Resources/input.txt";
+            Day18 day = new Day18();
+
+            // Act
+            long result = day.Compute2(filePath);
 
             // Result
             _output.WriteLine(result.ToString());
--- AdventOfCode/AdventOfCode/Day18/Day18.cs
+++ AdventOfCode/AdventOfCode/Day18/Day18.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Windows.Markup;
 using System.Xml;
 using AdventOfCode.Utils;
@@ -12,78 +13,148 @@ namespace AdventOfCode
 {
     public class Day18
     {
-        public class Node
+        public class Action
         {
-            public int Value { get; set; }
-            public Node Left { get; set; }
-            public Node Right { get; set; }
+            public int Type { get; set; }
+            public int Order { get; set; }
             public int Level { get; set; }
-        }
+        }  
         public long Compute(string filePath)
         {
             var input = FileReader.GetFileContent(filePath).ToList();
 
-            Node node = CreateNodeFromString(input[0], 1);
+            string finalChain = SumAll(input);
 
-            return 0;
+            return Magnitude(finalChain);
         }
 
-        private static void Reduce(Node node)
+        public long Compute2(string filePath)
         {
-            if (node.Level > 4 && node.Left.Value > 0 && node.Right.Value > 0)
+            var input = FileReader.GetFileContent(filePath).ToList();
+
+            long magnitudeMax = 0;
+
+            foreach(var chain in input)
             {
-
-            }
-        }
-
-        private static Node CreateNodeFromString(string chain, int level)
-        {
-            Node node = new Node();
-            node.Level = level;
-
-            if (chain.Length == 1)
-            {
-                node.Value = int.Parse(chain);
-                return node;
-            }
-
-            var asArray = chain.ToCharArray().ToList();
-            asArray.RemoveAt(0);
-            asArray.RemoveAt(asArray.Count - 1);
-
-            int countOpen = 0;
-            int countClose = 0;
-            string left = "";
-            string right = "";
-            for (int i=0; i<asArray.Count; i++)
-            {
-                if (asArray[i] == '[')
+                foreach(var addedChain in input)
                 {
-                    countOpen++;
-                } else if (asArray[i] == ']')
-                {
-                    countClose++;
-                }
-                if (node.Left == null)
-                {
-                    left += asArray[i].ToString();
-                } else
-                {
-                    right += asArray[i].ToString();
-                }
-                if (countOpen == countClose)
-                {
-                    if (node.Left == null)
+                    if (chain != addedChain)
                     {
-                        node.Left = CreateNodeFromString(left, node.Level + 1);
-                        i++;
-                    } else
-                    {
-                        node.Right = CreateNodeFromString(right, node.Level + 1);
+                        var result = Magnitude(Add(chain, addedChain));
+                        magnitudeMax = result > magnitudeMax ? result : magnitudeMax;
                     }
                 }
             }
-            return node;
+
+            return magnitudeMax;
         }
+
+        public static long Magnitude(string chain)
+        {
+            Regex regex = new Regex(@"\[([0-9]+),([0-9]+)\]");
+
+            while (regex.Matches(chain).Count > 0)
+            {
+                foreach(Match match in regex.Matches(chain))
+                {
+                    chain = chain.Replace(match.Value, (long.Parse(match.Groups[1].Value) * 3 + long.Parse(match.Groups[2].Value) * 2).ToString());
+                }
+            }
+            return long.Parse(chain);
+        }
+
+        public static string SumAll(List<string> chains)
+        {
+            string chain = chains[0];
+            foreach (string addChain in chains.Skip(1))
+            {
+                chain = Add(chain, addChain);
+            }
+            return chain;
+        }
+
+        public static string Add(string chain1, string chain2)
+        {
+            var chain = $"[{chain1},{chain2}]";
+            return CycleReduce(chain);
+            
+        }
+
+        public static string CycleReduce(string chain)
+        {
+            List<string> reductions = new List<string>();
+            var reducedChain = Reduce(chain);
+            while (chain != reducedChain)
+            {
+                chain = reducedChain;
+                reducedChain = Reduce(chain);
+                reductions.Add(chain);
+            }
+            return chain;
+        }
+
+        public static string Reduce(string chain)
+        {
+            var chainArray = new List<string>();
+
+            foreach(char c in chain.ToCharArray())
+            {
+                if (specificChar.Contains(c.ToString()) || specificChar.Contains(chainArray.Last()))
+                {
+                    chainArray.Add(c.ToString());
+                } else
+                {
+                    chainArray[chainArray.Count -1] += c.ToString(); 
+                }
+            }
+
+            for (int i=0; i< chainArray.Count; i++)
+            {
+                int countOpen = chainArray.Where((x, index) => x == "[" && index <= i).Count() - chainArray.Where((x, index) => x == "]" && index <= i).Count();
+                if (countOpen > 4 && chainArray[i+2] == "," && chainArray[i+4] == "]")
+                {
+                    int left = int.Parse(chainArray[i + 1]);
+                    int right = int.Parse(chainArray[i + 3]);
+                    for (int j = i + 5; j < chainArray.Count; j++)
+                    {
+                        if (!specificChar.Contains(chainArray[j]))
+                        {
+                            var newValue = int.Parse(chainArray[j]) + right;
+                            chainArray[j] = newValue.ToString();
+                            break;
+                        }
+                    }
+                    for (int j=i-1;j>=0;j--)
+                    {
+                        if (!specificChar.Contains(chainArray[j]))
+                        {
+                            var newValue = int.Parse(chainArray[j]) + left;
+                            chainArray[j] = newValue.ToString();
+                            break;
+                        }
+                    }
+                    chainArray[i] = "0";
+                    for (int j=0;j<4;j++)
+                    {
+                        chainArray.RemoveAt(i+1);
+                    }
+                }
+            }
+
+            for (int i= 0; i<chainArray.Count; i++)
+            {
+                if (chainArray[i].Length > 1)
+                {
+                    int value = int.Parse(chainArray[i]);
+                    chainArray.InsertRange(i + 1, new List<string> { "[", (value / 2).ToString(), ",", ((value / 2) + (value % 2)).ToString(), "]" });
+                    chainArray.RemoveAt(i);
+                    break;
+                }
+            }
+
+            return string.Join("", chainArray);
+        }
+
+        public static List<string> specificChar = new List<string> { "[", "]", "," };
     }
 }
