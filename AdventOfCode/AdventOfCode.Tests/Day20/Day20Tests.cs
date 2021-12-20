42fde2c763b2c0b7859cb2555ed3b04eef0162a3
maxime.andre <maxime.andr@gmail.com>
2021-12-20 10:16:53 +0100

    day20 part 1 & 2

---
+++ AdventOfCode/AdventOfCode.Tests/Day20/Day20Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day20Tests
+    {
+        private ITestOutputHelper _output;
+        public Day20Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day20/Resources/test.txt";
+            Day20 day = new Day20();
+
+            // Act
+            long result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(35);
+        }
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day20/Resources/input.txt";
+            Day20 day = new Day20();
+
+            // Act
+            long result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day20/Resources/test.txt";
+            Day20 day = new Day20();
+
+            // Act
+            long result = day.Compute(filePath, 50);
+
+            // Assert
+            result.Should().Be(3351);
+        }
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day20/Resources/input.txt";
+            Day20 day = new Day20();
+
+            // Act
+            long result = day.Compute(filePath, 50);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
